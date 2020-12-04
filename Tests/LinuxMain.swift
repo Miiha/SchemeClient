b349f6ade3f7b9a645574f15db1ed9d566461398
Michael Kao <micha.kao@posteo.de>
2020-12-04 21:06:07 +0100

    Removes tests.

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import SchemeClientTests
-
-var tests = [XCTestCaseEntry]()
-tests += SchemeClientTests.allTests()
-XCTMain(tests)
--- Tests/SchemeClientTests/SchemeClientTests.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-@testable import SchemeClient
-
-final class SchemeClientTests: XCTestCase {
-  func testExample() {
-  }
-}
